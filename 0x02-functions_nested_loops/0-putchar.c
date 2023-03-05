6b5ba4272f31e3d2105aa8c3f02611ed2c75e78b
TYNESHD <tinashedubem@gmail.com>
2023-03-05 06:00:30 -0800

    a program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,40 @@
+#include "main.h"
+/**
+ * A program that prints _putchar and a new line.
+ *
+ * This program demonstrates the use of the _putchar function to print
+ * characters to the console. The program prints the character '_' followed by
+ * the string "putchar" and a new line character '\n'.
+ *
+ * @return 0 on successful execution.
+ */
+
 #include <stdio.h>
 
-int main(void) {
-  printf("_putchar\n");
-  return 0;
+/**
+ * Prints a character to the console.
+ *
+ * This function writes a single character to the standard output stream using
+ * the putchar function.
+ *
+ * @param c The character to print.
+ *
+ * @return The written character if successful, otherwise EOF.
+ */
+int _putchar(char c) {
+    return putchar(c);
 }
+
+int main(void) {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return 0;
+}
+
