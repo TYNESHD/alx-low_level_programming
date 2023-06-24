0054bdf4b909e24fa7f20c3afe2b2b2e29cdacdc
Tinashe Dube <tinashedubem@gmail.com>
2023-06-24 05:12:19 -0700

    C - RecursionWhat is recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - Print a string, followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);        /* prints the last letter*/
+		_puts_recursion(s + 1);
+	}
 
+i	else
+		_putchar('\n');
+}
